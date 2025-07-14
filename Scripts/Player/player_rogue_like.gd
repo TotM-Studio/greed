711dd116b23ba308234009cf65c0443a1c55fab4
ToT M <robloxit14@gmail.com>
2025-07-14 22:21:49 +0200

    Le joueur RogueLike bouge youpiiiiii !

--- Scripts/Player/player_rogue_like.gd
+++ Scripts/Player/player_rogue_like.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+@export var GRAVITY : int = 100
+@export var acceleration : int = 1220
+@export var speed : int = 1110
+
+func _process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y += GRAVITY
+	
+	var direction = Input.get_axis("Gauche", "Droite")
+	
+	velocity.x = move_toward(velocity.x, direction * speed, acceleration)
+	
+	move_and_slide()
