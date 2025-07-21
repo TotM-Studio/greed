755f9073d9762b5189bcc2c1581aa9521c0a777a
ToT M <robloxit14@gmail.com>
2025-07-21 18:05:12 +0200

    ....

--- Scripts/Components/attack_component.gd
+++ Scripts/Components/attack_component.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 	connect("area_entered",on_area_entered)
 
 func on_area_entered(Area : HitBoxComponent) -> void :
-	if Area != null :
+	if Area != null and Area.has_method("take_dammage"):
 		print(Area.get_method_list())
