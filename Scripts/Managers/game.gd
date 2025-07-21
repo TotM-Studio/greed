5419380f4fe450bceaaefed841615b7a51fcd427
ToT M <robloxit14@gmail.com>
2025-07-21 10:03:47 +0200

    Modified Game/func/freeze_engine.freeze_slow = 0.5

--- Scripts/Managers/game.gd
+++ Scripts/Managers/game.gd
@@ -1,6 +1,6 @@
 extends Node
 
-func  freeze_engine(freezeTime:float, freezeSlow:float) -> void:
+func  freeze_engine(freezeTime:float, freezeSlow:float = 0.5) -> void:
 	Engine.time_scale = freezeSlow
 	await get_tree().create_timer(freezeTime * freezeSlow).timeout
 	Engine.time_scale = 1
