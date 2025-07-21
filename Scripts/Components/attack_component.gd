d420d1dbf34fad544dafeac4446b3e7381fa3a32
ToT M <robloxit14@gmail.com>
2025-07-21 15:09:40 +0200

    Ajout de la scène Weapon

---
+++ Scripts/Components/attack_component.gd
@@ -0,0 +1,11 @@
+extends Area2D
+class_name AttackComponent
+
+@export var dammages : float = 10
+
+func _ready() -> void:
+	connect("area_entered",on_area_entered)
+
+func on_area_entered(Area : HitBoxComponent) -> void :
+	if Area != null :
+		print(Area.get_method_list())
