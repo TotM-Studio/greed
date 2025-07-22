49bb1b486c69ac0555829797a6ee08578a351c61
ToT M <robloxit14@gmail.com>
2025-07-22 14:42:54 +0200

    ........

--- Scripts/Components/attack_component.gd
+++ Scripts/Components/attack_component.gd
@@ -2,18 +2,17 @@ extends Area2D
 class_name AttackComponent
 
 @export var dammages : float = 10
-@export var direction : Vector2 = Vector2.ZERO
+@export var knockback : Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	connect("area_entered",on_area_entered)
 
 func on_area_entered(Area : HitBoxComponent) -> void :
 	if Area != null and Area.has_method("take_dammage"):
-		var direction_2 = Vector2(direction.x * direction2(Area, self), direction.y)
-		Area.take_dammage(dammages, direction_2)
+		print(direction2(Area.parent, self))
+		var knockback_2 = knockback
+		knockback_2.x *= direction2(Area.parent, self)
+		Area.take_dammage(dammages, knockback_2)
 
 func direction2(chr1, chr2) -> int:
-	if chr1.position.x - chr2.position.x < 0:
-		return 1
-	else:
-		return -1
+	return max(min((chr1.position.x-chr2.position.x)*10, 1), -1)
