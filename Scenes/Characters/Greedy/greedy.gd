c040490002a870bfbb76fc42b72b88dcebe81e56
ToT M <robloxit14@gmail.com>
2025-07-28 20:44:15 +0200

    Aaaaaa

--- Scenes/Characters/Greedy/greedy.gd
+++ Scenes/Characters/Greedy/greedy.gd
@@ -14,6 +14,11 @@ func _physics_process(delta: float) -> void:
 func take_dammage(direction : Vector2 = Vector2.ZERO):
 	$AnimationPlayer.play("take_dmmage")
 	$CPUParticles2D.direction = direction.normalized()
+	$CPUParticles2D.emitting = true
+	$Punch.pitch_scale = randf()/2 + 0.9
+	$Punch.play()
+	if health <= 0:
+			$Aaaaaaaa.play()
 	Game.freeze_engine(0.2, 0.5)
 	Input.start_joy_vibration(1, 0.5, 0, 0.1)
 	Input.start_joy_vibration(0, 0.5, 0, 0.1)
