68c0bd910c5693684c68d59171c2768d1978ea8e
ToT M <robloxit14@gmail.com>
2025-07-28 16:32:35 +0200

    Ajout de l'animation de dammage du greedy

--- Scenes/Characters/Greedy/greedy.gd
+++ Scenes/Characters/Greedy/greedy.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @export var health = 100
 
 func _physics_process(delta: float) -> void:
-	velocity.y = move_toward(velocity.y, 0, 1000 * delta)
-	velocity.x = move_toward(velocity.x, 0, 1000 * delta)
+	velocity.y = move_toward(velocity.y, 0, 2000 * delta)
+	velocity.x = move_toward(velocity.x, 0, 2000 * delta)
 	move_and_slide()
+
+func take_dammage():
+	$AnimationPlayer.play("take_dmmage")
