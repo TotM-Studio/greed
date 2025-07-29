f12647afa68f7987a81bb22170d4ed229deac7b9
ToT M <robloxit14@gmail.com>
2025-07-29 12:09:18 +0200

    Début d'implémentation du changement d'items

--- Scenes/Characters/Player/weapons.gd
+++ Scenes/Characters/Player/weapons.gd
@@ -2,12 +2,17 @@ extends Node2D
 
 @onready var animation_player = $AnimationPlayer
 @export_node_path("Weapon") var current_weapon_path
+
+var weapons : Dictionary
 var current_weapon : Weapon
 
 var playing : bool = false
 
 func _ready() -> void:
 	current_weapon = get_node(current_weapon_path)
+	for i in $Pivot.get_children():
+		if i is Weapon:
+			weapons[i.name] = i
 
 func  _process(_delta: float) -> void:
 	playing = animation_player.is_playing()
