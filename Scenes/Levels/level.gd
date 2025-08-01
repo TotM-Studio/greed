6b97bdb2bc76909eb8b012a6db2b1e3e09e6af4a
ToT M <robloxit14@gmail.com>
2025-08-01 09:30:45 +0200

    Amélioration de la transition de niveaux

--- Scenes/Levels/level.gd
+++ Scenes/Levels/level.gd
@@ -4,6 +4,6 @@ extends Node2D
 @onready var spawners = $Spawners
 
 func _ready() -> void:
-	TransitionManager._in()
+	TransitionManager._in(1.5)
 	if Game.spawner <= spawners.get_child_count():
 		player.position = spawners.get_child(Game.spawner).position
